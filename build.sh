9148ff84a1c82ec4ff8d1b4de0b71fa07a484460
KK <kiranops@yahoo.com>
2021-08-10 07:27:06 +0530

    updted Az Subscription

--- build.sh
+++ build.sh
@@ -2,11 +2,9 @@
 
 chmod 400 id_rsa
 
-
 echo "Create hosts file"
 echo "[webservers]" > hosts 
-az vm list --resource-group Victory-rg --show-details --query "[?contains(name, 'web')].privateIps" -otable | tail +3 >> hosts  
-
+az vm list --resource-group Victory-rg --show-details --query "[?contains(name, 'web')].privateIps" -otsv >> hosts  
 
 echo "Run Ansible Playbook"
 ansible-playbook -i hosts tomcat_playbook.yaml --private-key id_rsa
