75e20eb4e78fff83ad68667c464c9633d399cec7
KK <kiranops@yahoo.com>
2021-08-10 06:50:29 +0530

    updated priv key

--- build.sh
+++ build.sh
@@ -2,12 +2,13 @@
 
 ls -l .
 ls -l ~/.ssh/
-cat id_demo
-cat ~/.ssh/id_demo
+cat id_rsa
+cat ~/.ssh/id_rsa
+chmod 400 id_rsa
 
 echo "[webservers]" > hosts 
 az vm list --resource-group Victory-rg --show-details --query "[?contains(name, 'web')].privateIps" -otable | tail +3 >> hosts  
 
 
 
-ansible-playbook -i hosts tomcat_playbook.yaml 
+ansible-playbook -i hosts tomcat_playbook.yaml --private-key id_rsa
