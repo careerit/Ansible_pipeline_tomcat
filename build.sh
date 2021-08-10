b53d6bc8e34cb3e2df7e0eeed13eb729a2f16f13
KK <kiranops@yahoo.com>
2021-08-10 06:56:14 +0530

    removed test steps

--- build.sh
+++ build.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
-ls -l .
-ls -l ~/.ssh/
-cat id_rsa
-cat ~/.ssh/id_rsa
-chmod 400 id_rsa
 
+echo "Create hosts file"
 echo "[webservers]" > hosts 
 az vm list --resource-group Victory-rg --show-details --query "[?contains(name, 'web')].privateIps" -otable | tail +3 >> hosts  
 
 
-
+echo "Run Ansible Playbook"
 ansible-playbook -i hosts tomcat_playbook.yaml --private-key id_rsa
