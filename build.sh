e252536f49809311e53cc3a441c54c9f7dbec69a
Kiran <kiranops@yahoo.com>
2021-10-28 08:07:47 +0530

    used variable for resource group

--- build.sh
+++ build.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+RESOURCE_GROUP=$1
 
 chmod 400 id_rsa
 
 echo "Create hosts file"
 echo "[webservers]" > hosts 
-az vm list --resource-group Victory-rg --show-details --query "[?contains(name, 'web')].privateIps" -otsv >> hosts  
+az vm list --resource-group $RESOURCE_GROUP --show-details --query "[?contains(name, 'web')].privateIps" -otsv >> hosts  
 
 echo "Run Ansible Playbook"
 ansible-playbook -i hosts tomcat_playbook.yaml --private-key id_rsa
