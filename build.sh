014a1754a3092c22b062abffb90ac25a4af2efa2
Kiran <kiranops@yahoo.com>
2021-12-18 09:22:52 +0530

    updated Resoruce Group

--- build.sh
+++ build.sh
@@ -5,7 +5,8 @@ chmod 400 id_rsa
 
 echo "Create hosts file"
 echo "[webservers]" > hosts 
-az vm list --resource-group $RESOURCE_GROUP --show-details --query "[?contains(name, 'web')].privateIps" -otsv >> hosts  
+az vm list --resource-group $RESOURCE_GROUP --show-details \
+           --query "[?contains(name, 'web')].privateIps" -otsv >> hosts  
 
 echo "Run Ansible Playbook"
 ansible-playbook -i hosts tomcat_playbook.yaml --private-key id_rsa
