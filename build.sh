35be95554a5c0d2460bf45cbc0cd375ef28a9450
kiran <kiranops@yahoo.com>
2022-02-09 22:10:55 +0530

    disabled host key checking

--- build.sh
+++ build.sh
@@ -3,6 +3,11 @@ RESOURCE_GROUP=$1
 
 chmod 400 id_rsa
 
+echo -e "Host 10.*\n \tStrictHostKeyChecking no" > ~/.ssh/config
+
+cat ~/.ssh/config 
+
+
 echo "Create hosts file"
 echo "[webservers]" > hosts 
 az vm list --resource-group $RESOURCE_GROUP --show-details \
